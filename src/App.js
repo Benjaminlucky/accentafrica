1bc6350cf755b96aa6e307099cd94b96fb175df3
Benjaminlucky <42000766+Benjaminlucky@users.noreply.github.com>
2024-05-09 21:20:25 +0100

    Bottom footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import MainNav from "./components/mainnav/MainNav";
 import { Helmet } from "react-helmet";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>} />
           </Routes>
+          <Footer />
         </Router>
     </div>
   );
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+  return (
+    <div className="footer__section">
+        <div className="footer__wrapper">
+            <div className="footer__bottom">
+                <div className="bottom__content">
+                    <div className="bottom__left">
+                        <p>&copy; All Rights Reserved Accent Africa Alliance Limited</p>
+                    </div>
+                    <div className="bottom__right">
+                        <p>Developed by Inspireme Media Networks</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
